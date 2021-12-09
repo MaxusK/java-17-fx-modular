3a87fc76c49210c064021098bae9fe9d4e7de054
Max Knauber <maxknauber@gmail.com>
2021-12-09 19:47:06 +0100

    Calculator und CalculatorTest aktualisiert

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -3,18 +3,18 @@ package com.example.calculator;
 public class Calculator {
     public double add(double number1, double number2) {
         // return To do
-        return 0.0;
+        return number1+number2;
     }
     public double subtract(double number1, double number2) {
         // return To do
-        return 0.0;
+        return number1-number2;
     }
     public double multiply(double number1, double number2) {
         // return To do
-        return 0.0;
+        return number1*number2;
     }
     public double divide(double number1, double number2) {
         // return To do
-        return 0.0;
+        return number1/number2;
     }
 }
--- src/test/java/com/example/calculator/CalculatorTest.java
+++ src/test/java/com/example/calculator/CalculatorTest.java
@@ -17,6 +17,13 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        assertEquals (c.add(0.0,0.0), 0.0, 0.01);
+        assertEquals (c.add(4.0,4.0), 8.0, 0.01);
     }
+    @Test
+    public void testSubtract(){assertEquals(c.subtract(4.0,4.0),0.0,0.01); }
+    @Test
+    void testMultiply() {assertEquals(c.multiply(4.0,4.0),16.0,0.01);}
+    @Test
+    void testDivide() {assertEquals(c.divide(4.0,4.0),1,0.01);}
 }
+
